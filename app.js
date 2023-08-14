07b5cb00fb1863d5ba185d372c3e3b363ec562e1
Aquit Tania <82707904+lumstu1024@users.noreply.github.com>
2023-08-14 14:33:29 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'8141d7a2-a0ec-4567-cd94-8aff515dd014').replace(/-/g, "");
+const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
